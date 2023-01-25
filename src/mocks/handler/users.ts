7c8d3feb4172f973329c1191b48fa4031fb5c978
minyoung <minsnew@naver.com>
2023-01-25 17:58:57 +0900

    feat: add user mock data

--- src/mocks/handler/users.ts
+++ src/mocks/handler/users.ts
@@ -1,8 +1,8 @@
-import casual from 'casual-browserify';
 import { rest } from 'msw';
+import { MockUserListData } from '../dummy/user';
 
 export const userHandlers = [
   rest.get('/api/users', async (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ name: casual.name }));
+    return res(ctx.status(200), ctx.json(MockUserListData));
   }),
 ];
