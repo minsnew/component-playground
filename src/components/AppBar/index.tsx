a73276daeca05739dabdb17eef3d934e947325b4
minsnew <minsnew@naver.com>
2023-04-09 13:03:12 +0900

    Add side bar component (#7)
    * feat: add react icons
    
    * feat: add classnames library
    
    * feat: add side bar component
    
    * feat: reflect sidebar in app bar

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,6 +1,17 @@
+import classNames from 'classnames'
+import SideBar from '../SideBar'
+
 const AppBar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full p-4 bg-white border-b border-gray-400">
+    <div
+      className={classNames(
+        'fixed top-0 left-0',
+        'flex w-full p-4',
+        'bg-white',
+        'border-b border-gray-400'
+      )}
+    >
+      <SideBar />
       <h1 className="text-[20px] font-bold">Component Playgound</h1>
     </div>
   )
