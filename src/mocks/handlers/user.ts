db77ffb70bc0fc6f150b9bb0425964b711573783
minyoung <minsnew@naver.com>
2023-04-09 09:11:45 +0900

    feat: reflect mock users in msw user handler

--- src/mocks/handlers/user.ts
+++ src/mocks/handlers/user.ts
@@ -1,7 +1,8 @@
 import { rest } from 'msw'
+import { generateUsers } from '../data/user'
 
 export const userHandlers = [
   rest.get('/users', (_, res, ctx) => {
-    return res(ctx.status(200), ctx.json({}))
+    return res(ctx.status(200), ctx.json(generateUsers()))
   }),
 ]
