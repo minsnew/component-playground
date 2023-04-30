2c27746a747609cdfcfbbb6e4739743f28dacf23
minyoung <minsnew@naver.com>
2023-05-01 08:59:59 +0900

    feat: add color checker page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/Main'
 import NotFoundPage from './pages/NotFound'
+import { ROUTER_URL } from './common/consts/url'
+import ColorCheckerPage from './pages/ColorChecker'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path={ROUTER_URL.ROOT} element={<MainPage />} />
+        <Route path={ROUTER_URL.COLOR_CHECKER} element={<ColorCheckerPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
